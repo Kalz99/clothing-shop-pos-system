f981a31d64b8ebd50104fe466db8916c51ea7e1f
Kalhara Gunawardhana <kalzmiami@gmail.com>
2026-02-19 14:23:40 +0530

    removed live backend link

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://lazaro.luxn.lk/api',
+    baseURL: '',
     headers: {
         'Content-Type': 'application/json',
     },
